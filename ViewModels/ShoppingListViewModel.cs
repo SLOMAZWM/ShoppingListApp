2ab8b78777c18f5237e400b06b13bf28735343ac
Marcin Slomka <m.slomka@student.po.edu.pl>
2024-04-20 23:01:45 +0200

    Add new image to add button to navigato to new content with add ShoppingList

---
+++ ViewModels/ShoppingListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AppMVVM.ViewModels
+{
+    public class ShoppingListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
